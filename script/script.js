65949b1a7d35ffc2a4a3f97b1305420d5cbbe366
Joran Schrievers <99062987@mydavinci.nl>
2022-02-17 14:36:56 +0100

    Started with writing the script

--- script/script.js
+++ script/script.js
@@ -1,1 +1,7 @@
-let playersArray = [{img:'images/Burger.jpg', name:'Wouter Burger'}, {img:'images/Katterbach.jpg', name:'Noah Katterbach'}, {img:'images/Lang.jpg', name:'Michael Lang'}]
+let playersArray = [{img:'images/Burger.jpg', name:'Wouter Burger'}, {img:'images/Katterbach.jpg', name:'Noah Katterbach'}, {img:'images/Lang.jpg', name:'Michael Lang'}, {img:'images/Lidner.jpg', name:'Heinz Lindner'}, {img:'images/Djiga.jpg', name:'Yacouba Nasser Djiga'}, {img: 'images/Kasami.jpg', name:'Pajtim Kasami'}, {img:'images/Fernandes.jpg', name:'Joelson Fernandes'}, {img:'images/gebhardt.jpg', name:'Felix Gebhardt'}, {img:'images/nikolic.jpg', name:'Djorde Nikolic'}, {img:'images/Millar.jpg', name:'Liam Millar'}];
+
+const playerPic = document.getElementById('playerpic');
+
+function randomPic(){
+	
+}
